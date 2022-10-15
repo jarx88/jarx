1a429dafb12b6a4510540cb744f6e29ae718545b
jarx88 <jarekhart@gmail.com>
2022-10-15 16:51:25 +0200

    Update menu.js

--- data/menu.js
+++ data/menu.js
@@ -1,7 +1,7 @@
 const menu = [
   { name: "Start", path: "/" },
   // { name: "Projects", path: "/projects" },
-  { name: "Blog", path: "https://blog.jarx.pl" },
+  { name: "Blog", url: "https://blog.jarx.pl" },
 ];
 
 export default menu;
