3ec0c1b818c4f0d46d9daf24117278dea0039349
jarx88 <jarekhart@gmail.com>
2022-10-15 15:04:39 +0200

    Update TimeLine.vue

--- components/TimeLine.vue
+++ components/TimeLine.vue
@@ -94,7 +94,7 @@
           Inżynier
         </p>
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          Informatyka – Przemysłowe Systemy Informatyczne - inżynier
+          Informatyka – Przemysłowe Systemy Informatyczne
         </p>
       </li>      
       <li class="mb-10 ml-6">
