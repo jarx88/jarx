963e2b9c02c7db6836a0218346e440dcbd002095
jarx88 <jarekhart@gmail.com>
2022-10-15 13:45:56 +0200

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,21 +2,14 @@
   <footer>
     <div class="flex flex-col items-center mt-16">
       <div class="flex mb-3 space-x-4">
-        <a
-          class="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          :href="siteMetadata.github"
-          ><span class="sr-only">github</span>
-          <img class="w-8 h-8" src="~assets/icon/github_new.svg" /> </a
-        ><a
+        <!-- <a
           class="text-sm text-gray-500 transition hover:text-gray-600"
           target="_blank"
           rel="noopener noreferrer"
           :href="siteMetadata.linkedin"
           ><span class="sr-only">Linkedin</span>
-          <img class="w-8 h-8" src="~assets/icon/linkeding.svg" /> </a
-        ><a
+          <img class="w-8 h-8" src="~assets/icon/linkeding.svg" /> </a> -->
+        <a
           class="text-sm text-gray-500 transition hover:text-gray-600"
           target="_blank"
           rel="noopener noreferrer"
@@ -31,20 +24,20 @@
           ><span class="sr-only">facebook</span>
           <img class="w-8 h-8" src="~assets/icon/facebook.svg"
         /></a>
-        <a
+        <!-- <a
           class="text-sm text-gray-500 transition hover:text-gray-600"
           target="_blank"
           rel="noopener noreferrer"
           :href="siteMetadata.youtube"
           ><span class="sr-only">youtube</span>
           <img class="w-8 h-8" src="~assets/icon/youtube.svg"
-        /></a>
+        /></a> -->
       </div>
       <div class="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
         <div>Copyright © {{ new Date().getFullYear() }}</div>
         <div>•</div>
         <a href="/"
-          >{{ siteMetadata.author }}'s blog - {{ siteMetadata.slogan }}</a
+          >{{ siteMetadata.author }} - {{ siteMetadata.slogan }}</a
         >
       </div>
       <div class="mb-8 text-sm text-gray-500 dark:text-gray-400">
@@ -61,13 +54,13 @@
 
 <script>
 import siteMetaInfo from "@/data/sitemetainfo";
-import BuyMeACoffee from "./BuyMeACoffee.vue";
+// import BuyMeACoffee from "./BuyMeACoffee.vue";
 export default {
   data() {
     return {
       siteMetadata: siteMetaInfo,
     };
   },
-  components: { BuyMeACoffee },
+  // components: { BuyMeACoffee },
 };
 </script>
