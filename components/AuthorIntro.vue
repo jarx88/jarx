47a10aaf419e4ca2cbefd841a77bfcbce199cd00
jarx88 <jarekhart@gmail.com>
2022-12-21 19:49:55 +0100

    update

--- components/AuthorIntro.vue
+++ components/AuthorIntro.vue
@@ -6,14 +6,13 @@
       Witam🖖
     </h2>
     <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
-    Informatyka jest moją pasją i zajmuje się nią od najmłodszych lat. Dzięki temu posiadam bogaty wachlarz doświadczeń. Chcąc poszerzyć swoje zainteresowania wybrałem kierunek studiów związany z moją pasją.
-    Od 2019 jestem specjalistą od sp. Informatyki w mini korporacji. Do zakresu moich obowiązków należy administrowanie Serwerów opartych na linuxie ponad 10 maszyn. Systemy zostały własnoręcznie wdrożone od zera w serowni Oracle. Administruje bardzo dużą bazę PostgreSQL. Dodatkowo jestem administratorem Active Directory. Zajmuje się bezpieczeństwem sieci i konfiguracją sieci wraz z rozwiązaniami VPN dla pracy zdalnej. Dodatkowo jestem osobą odpowiedzialny za wsparcie zdalne ponad 500 komputerów. W celu usprawnienia pracy korporacji napisałem kilkanaście programów do wsparcia i automatyzacji działu zakupów i działu księgowego.
-
-    Zajmuje się dodatkowo tworzeniem stron internetowych. Napisałem kilka programów w środowisku skryptowym, w tym kilka botów.
-
-    Jestem osobą bardzo spokojną i opanowaną, dlatego wspaniale się dogaduję
-    z innymi. Współpraca ze mną zawsze jest na najwyższym poziomie.
-     <vue-typed-js :strings="['programować⌨️', 'komputery💻','Windows🪟','Linux🐧'] " :loop="true" :fadeOutDelay="1000" :typeSpeed="100">
+		Informatyka jest moją pasją i zajmuję się nią od najmłodszych lat. Dzięki temu posiadam bogaty wachlarz doświadczeń. Chcąc poszerzyć swoje zainteresowania, wybrałem kierunek studiów związany z moją pasją. Od 2019 roku jestem specjalistą ds. informatyki w mini korporacji. Do zakresu moich obowiązków należy administrowanie serwerów opartych na Linuksie, na ponad 10 maszynach. Systemy zostały własnoręcznie wdrożone od zera w serowni Oracle. Administruję bardzo dużą bazą PostgreSQL. Dodatkowo jestem administratorem Active Directory. Zajmuję się bezpieczeństwem sieci i konfiguracją sieci wraz z rozwiązaniami VPN dla pracy zdalnej. Dodatkowo jestem osobą odpowiedzialną za wsparcie zdalne ponad 500 komputerów. W celu usprawnienia pracy korporacji napisałem kilkanaście programów do wsparcia i automatyzacji działu zakupów i działu księgowego.</p>
+<p>
+Zajmuję się dodatkowo tworzeniem stron internetowych. Napisałem kilka programów w środowisku skryptowym, w tym kilka botów.
+</p>
+<p>
+Jestem osobą bardzo spokojną i opanowaną, dlatego wspaniale się dogaduję z innymi. Współpraca ze mną zawsze jest na najwyższym poziomie.
+     <vue-typed-js :strings="['programować⌨️', 'komputery💻','Windows🪟','Linux🐧','Ai🧠'] " :loop="true" :fadeOutDelay="1000" :typeSpeed="100">
       <h1>Kocham <span class="typing"></span></h1>
     </vue-typed-js>
     </p>
